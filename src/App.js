34cf50e1a40406e696145d7f0a8e2500f4c3541e
PoojaDL <poojadl2002@gmail.com>
2023-07-30 16:22:41 +0530

    Added meals Summary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
-  return <Header />;
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
